6b83bbdffa716493ec06d4ca06b1f2b82c22415e
Hudhayfee Hassan <HHassan@spartaglobal.com>
2019-07-25 10:59:08 +0100

    started the class Animal

--- venv/oop_animal_class.py
+++ venv/oop_animal_class.py
@@ -1,7 +1,14 @@
+class Animal(): 
+# Class are the basis of OOP - Object Orientated Programming
+# They are cookie cutters for objects.***
+# From classes you initilaise the individual instances of this class
+# Classes hold method
+# methods are function that depend on instances of classes
+# They can ONLY be called on instances of a class
 
-def sleep:
-    return 'zzzz'
+    def sleep(self):
+        return 'zzzz'
 
-def eat(food):
-    return 'nom NOM nom NOM this was some good ' + food
+    def eat(self, food):
+        return 'nom NOM nom NOM this was some good ' + food
 
